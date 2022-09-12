4a943d52eb76a51b0946f52040d84077a67a82fa
Lama Radwan <lama.radwan96@gmail.com>
2022-09-12 14:31:30 +0300

    added the basic layout and homepage styling

---
+++ sehtak/components/Navbar.js
@@ -0,0 +1,22 @@
+//Import Components
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
+import Dropdown from './Dropdown'
+
+
+export default function Navbar(){
+    return(
+        <>
+        <div className={styles.container}>
+        <Image src='/images/logo.png' alt='' width='210' height='80'/>
+        <div className={styles.items}>
+        <Link href='#'><a className={styles.item}>Home</a></Link>
+        {/* <Link href='#'><a className={styles.item}>Medical Centers</a></Link> */}
+        <Dropdown/>
+        <button className={styles.whiteButtons}>Login</button>
+        </div>
+        </div>
+        </>
+    )
+}
---
+++ sehtak/components/Services.js
@@ -0,0 +1,40 @@
+//Import Components
+import Link from 'next/link'
+import styles from '../styles/Services.module.css'
+import Image from 'next/image'
+
+export default function Services(){
+    return(
+        <>
+        <div className={styles.services}>
+        <h2 className={styles.h2}>Our Services</h2>
+        <div className={styles.sersec}>
+        <div className={styles.seicon}>
+            <p className={styles.p}>Create Doctor Visits</p>
+            <div className={styles.img}>
+            <Image src='/images/doctor.jpg' alt='' width='220' height='220'/>
+            </div>
+        </div>
+        <div className={styles.seicon}>
+            <p className={styles.p}>Serve your Medicines</p>
+            <div className={styles.img}>
+            <Image src='/images/medicines.png' alt='' width='220' height='220'/>
+            </div>
+        </div>
+        <div className={styles.seicon}>
+            <p className={styles.p}>Check your Tests Results</p>
+            <div className={styles.img}>
+            <Image src='/images/tests.jpeg' alt='' width='220' height='220'/>
+            </div>
+        </div>
+        <div className={styles.seicon}>
+            <p className={styles.p}>Check your X-rays</p>
+            <div className={styles.img}>
+            <Image src='/images/xray.png' alt='' width='220' height='220'/>
+            </div>
+        </div>
+        </div> 
+        </div>
+        </>
+    )
+}
