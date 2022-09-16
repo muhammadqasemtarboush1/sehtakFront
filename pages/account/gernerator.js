285d63075f45f6567017c7b50ab0b4ef28824fff
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-09-16 12:28:19 +0300

    styling the signin login pagest with the functinalities

--- pages/account/gernerator.js
+++ pages/account/gernerator.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuth } from '../../contexts/Auth';
 import { useRouter } from 'next/router';
 
+import {PatientProfile } from '../../components/ProfileComponents/Patient/PatientProfile'
 
 
 function App() {
@@ -16,13 +17,9 @@ function App() {
         router.push('login');
     }
     return (
-        <div className="pb-2 mx-auto bg-white shadow-xl rounded-2xl md:w-1/2">
-            <h1>Hi ...</h1>
-
-             {
-             isLoggedIn && <button className="bg-slate-600 " onClick={handleLogout}>LogOut</button>
-             }  
-        </div>
+            <>
+                <PatientProfile/>
+            </>
     );
 }
 
--- pages/account/signup.js
+++ pages/account/signup.js
@@ -1,57 +1,112 @@
+// https://jasonwatmore.com/post/2021/09/03/next-js-form-validation-example-with-react-hook-form
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useAuth } from '../../contexts/Auth';
-import Layout from '../../components/Steppers/InputLayout';
 
+
+import Image from 'next/image';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 export default function Signup() {
+    
+    // form validation rules 
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+        password: Yup.string()
+            .min(8, 'Password must be at least 8 characters')
+            .required('Password is required'),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+            .required('Confirm Password is required'),
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
+
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, reset, formState } = useForm(formOptions);
+    const { errors } = formState;
+
 
     const { signup } = useAuth();
     const router = useRouter();
 
-    function handleSignup(e) {
-        e.preventDefault();
+    function handleSignup(data) {
         let userInput = {
-            email: e.target.email.value,
-            password: e.target.password.value,
-            first_name: e.target.firstname.value,
-            last_name: e.target.lastname.value
+            email: data.email,
+            password: data.password
         }
         signup(userInput);
         router.push('/gernerator');
     }
     return (
         <>
-            <div className="pb-2 mx-auto bg-gray-100 shadow-2xl rounded-2xl md:w-1/2">
-
-                <div className="flex flex-col p-10 mt-9">
-                    <form onSubmit={handleSignup}>
-                        <Layout label="Email">
-                            <input name="email" type="email" className="inputField" />
-                        </Layout>
-                        <Layout label="password">
-                            <input type="password" name="password" className="inputField" />
-                        </Layout>
-                        <Layout label="First Name">
-                            <input name="firstname" className="inputField" />
-                        </Layout>
-                        <Layout label="Last Name">
-                            <input name="lastname" className="inputField" />
-                        </Layout>
-                        <div className="btnContainer">
-                            <button type="submit" className="btn" > Sign Up </button>
-                        </div>
-                    </form>
-                </div>
-                <p className="mb-6 ml-6 text-sm text-gray-500"
-                >Already has an acoount?
-                    <Link href="login">
-                        <a className="pl-6">Log In</a>
-                    </Link>
-
-                </p>
+        <div className='backGroundCoverImage'>
+            <div className='flex h-20 py-3 bg-gradient-to-r from-teal-100 to-teal-50 pl-14'> 
+                <Image src="/images/logo.png" width={140} height={30} className='ml-3'/> 
             </div>
+            
+            <main className='pl-8'>                    
+                    <div class="bg-grey-lighter min-h-screen flex flex-col">
+                    <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-1">
+                        <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                        <form onSubmit={handleSubmit(handleSignup)}>
+                            <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+                            <div className="text-red-600">{errors.email?.message}</div>
+                            <input
+                                {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`+"block border border-grey-light w-full p-3 rounded mb-3"}
+                                type="text"
+                                name="email"
+                                placeholder="Email" />
+                            
+                            <div className="text-red-600">{errors.password?.message}</div>
+                            <input 
+                                {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`+"block border border-grey-light w-full p-3 rounded mb-3"} 
+                                type="password"
+                                name="password"
+                                placeholder="Password" />
+                                
+                            <div className="text-red-600">{errors.confirmPassword?.message}</div>
+                            <input 
+                                {...register('confirmPassword')} className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`+"block border border-grey-light w-full p-3 rounded mb-3"}
+                                type="password"
+                                
+                                name="confirmPassword"
+                                placeholder="Confirm Password" />
+                                                      
 
+                            <button
+                                type="submit"
+                                class="w-full text-center py-3 rounded bg-blue-500 text-white hover:bg-blue-600 focus:outline-none my-1"
+                            >Create Account</button>
+
+                            <div class="text-center text-sm text-grey-dark mt-4">
+                                By signing up, you agree to the 
+                                <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                                    Terms of Service
+                                </a> and 
+                                <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                                    Privacy Policy
+                                </a>
+                            </div>
+                        </form>
+                        </div>
+                        <div class="text-grey-dark mt-6">
+                            Already have an account? 
+                            <a class="no-underline border-b border-blue text-blue" href="login">
+                                Log in
+                            </a>.
+                        </div>
+                    </div>
+                </div>
+        
+            
+            </main>
+
+        </div>
         </>
     )
 }
