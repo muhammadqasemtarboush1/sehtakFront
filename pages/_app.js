da4a47d3a14c693f567e28d8da8ccb559087a092
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-09-16 14:40:15 +0300

    merge with develop

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ import { AuthProvider } from '../contexts/Auth';
 
 Router.events.on('routeChangeStart' , ()=> NProgress.start());
 Router.events.on('routeChangeComplete', ()=> NProgress.done());
-Router.events.on('r', ()=> NProgress.done());
+Router.events.on('routeChangeComplete', ()=> NProgress.done());
 
 
 function MyApp({ Component, pageProps }) {
