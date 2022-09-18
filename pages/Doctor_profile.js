3fa35f343c5ccac09f653f07f45231fc3257fb78
Lama Radwan <lama.radwan96@gmail.com>
2022-09-18 11:08:48 +0300

    styling

--- pages/Doctor_profile.js
+++ pages/Doctor_profile.js
@@ -1,14 +1,16 @@
 
 import Navbar from '../components/Navbar'
 import styles from '../styles/Professional.module.css'
+import Footer from '../components/Footer'
 
 export default function Doctor_Profile(){
     return(
         <>
         <Navbar/>
         <main className={styles.main}>
-
+            
         </main>
+        <Footer/>
         </>
     )
 }
