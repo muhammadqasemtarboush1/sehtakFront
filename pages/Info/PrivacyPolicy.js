7ea0bc84848032359cd0db718072c81bc9d82ba6
Lama Radwan <lama.radwan96@gmail.com>
2022-09-20 16:05:06 +0300

    added privacy policy page

--- pages/Info/PrivacyPolicy.js
+++ pages/Info/PrivacyPolicy.js
@@ -1,21 +1,42 @@
+import Footer from "../../components/Footer";
+import styles from '../../styles/Home.module.css'
 import Navbar from "../../components/Navbar";
 
 
 
-export default function PrivacyPolicy(){
-    return(
+export default function PrivacyPolicy() {
+    return (
         <>
-        <Navbar/>
-        <h1>TERMS OF USE</h1>
-        <p>
-        These Website Terms of Use (“Agreement” or “Terms of Use”) are a legally binding agreement between you (the “User,” “you,” or “your”) and Aumet, Inc. (“Aumet,” “we,” “us,” “our”). You acknowledge and agree that your use of the Aumet platform (the “Platform”) through Aumet’s website at www.aumet.com (the “Website”) will be governed by this Agreement, our Privacy Policy, our Acceptable Use Policy and any related terms.
+            <Navbar />
+            <p className={styles.titlo}>TERMS OF USE</p>
+            <p className={styles.paragraph}>
+                These Website Terms of Use (“Agreement” or “Terms of Use”) are a legally binding agreement between you (the “User,” “you,” or “your”) and Sehtak, Inc. (“Sehtak,” “we,” “us,” “our”). You acknowledge and agree that your use of the Sehtak platform (the “Platform”) through Sehtak’s website(the “Website”) will be governed by this Agreement, our Privacy Policy, our Acceptable Use Policy and any related terms.
 
-If you are unsure as to the terms of this Agreement, please do not proceed further and contact us at info@sehtak.com
+                If you are unsure as to the terms of this Agreement, please do not proceed further and contact us at info@sehtak.com
 
-Your use of our Website or Platform shall constitute your acceptance of this Agreement and to all of the terms and conditions stated under this Agreement and our Privacy Policy and Acceptable Use Policy referenced herein.
+                Your use of our Website or Platform shall constitute your acceptance of this Agreement and to all of the terms and conditions stated under this Agreement and our Privacy Policy and Acceptable Use Policy referenced herein.
 
-PLEASE READ THE TERMS CONTAINED IN THIS AGREEMENT CAREFULLY TO ENSURE THAT YOU UNDERSTAND EACH PROVISION. PLEASE NOTE THAT THESE TERMS CONTAIN A BINDING AND MANDATORY ARBITRATION PROVISION AND CLASS ACTION/JURY TRIAL WAIVER PROVISION THAT REQUIRES THE USE OF ARBITRATION ON AN INDIVIDUAL BASIS TO RESOLVE DISPUTES, RATHER THAN JURY TRIALS OR CLASS ACTIONS AND LIMITS REMEDIES AVAILABLE TO YOU IN THE EVENT OF CERTAIN DISPUTES.
-        </p>
+                PLEASE READ THE TERMS CONTAINED IN THIS AGREEMENT CAREFULLY TO ENSURE THAT YOU UNDERSTAND EACH PROVISION. PLEASE NOTE THAT THESE TERMS CONTAIN A BINDING AND MANDATORY ARBITRATION PROVISION AND CLASS ACTION/JURY TRIAL WAIVER PROVISION THAT REQUIRES THE USE OF ARBITRATION ON AN INDIVIDUAL BASIS TO RESOLVE DISPUTES, RATHER THAN JURY TRIALS OR CLASS ACTIONS AND LIMITS REMEDIES AVAILABLE TO YOU IN THE EVENT OF CERTAIN DISPUTES.
+            </p>
+            <p className={styles.titlo}>PRIVACY POLICY</p>
+            <p className={styles.paragraph}>
+                Our PRIVACY POLICY describes how we handle the personal and business information you provide to us when you register for our Website or Platform. You understand that through your use of our Website or Platform, you consent to the collection and use (as set forth in the PRIVACY POLICY) of this information, including the transfer of this information to the US, and/or other countries for storage, processing and use by Sehtak and our affiliates.
+            </p>
+            <p className={styles.titlo}>
+            ELIGIBILITY & ACCESS RESTRICTIONS
+            </p>
+            <p className={styles.paragraph}>
+                To be eligible to use our Website or Platform, you must meet the following criteria and represent and warrant that you: (a) are 18 years of age or older; (b) are not currently restricted from accessing our Website or Platform, or not otherwise prohibited from having an account, (c) are not our competitor, or are not using our Website or Platform for reasons that are in competition with us; (d) will only maintain one registered account at any given time; (e) have full power and authority to enter into this Agreement and doing so will not violate any other agreement to which you are a party; (f) will not violate any of our rights, including intellectual property rights such as patent, copyright, and trademark rights; and (g) agree to provide at your cost all equipment, browser software, and internet access necessary to use our Website or Platform.
+            </p>
+            <p className={styles.titlo}>ACCESS AND SERVICE RESTRICTIONS</p>
+            <p className={styles.paragraph}>
+                You agree that our Platform, including but not limited to the Website, graphics, trademarks, and editorial content, contains proprietary content, information, and material, which are owned by Sehtak and/or our licensors, including our customers, brands and agencies, and are protected by applicable intellectual property and other laws, including but not limited to copyright. You agree that you will not use such proprietary content, information or materials other than for your permitted use of our Platform or in any manner that is inconsistent with the terms contained in this Agreement.
+
+                You agree not to modify, rent, lease, loan, sell, distribute, or create derivative works based on our Website or Platform, in any manner, and you will not exploit our Website or Platform in any unauthorized way whatsoever, including but not limited to, using our Website or Platform to transmit any computer viruses, trojans, worms, logic bombs, or other material that is malicious or technologically harmful, or by trespassing or burdening network capacity. You further agree not to use our Website or Platform in any manner to harass, abuse, stalk, threaten, defame, or otherwise infringe or violate the rights of any other party, and that we are not in any way responsible for any such use by you, nor for any harassing, threatening, defamatory, offensive, infringing or illegal messages or transmissions that you may receive as a result of using our Website or Platform.
+
+                You must not attempt to gain unauthorized access to our Website and Platform, the server on which our site is stored, or any server, computer, or database connected to our site. You must not attack our site via a denial-of-service attack or a distributed denial-of service attack. By breaching this provision, you would commit a criminal offence under the Computer Misuse Act 1990. We will report any such breach to the relevant law enforcement authorities, and we will co-operate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use our Website and Platform will cease immediately.
+            </p>
+            <Footer/>
         </>
     )
 }
